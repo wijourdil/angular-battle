50cec789f76228e6a52b9fcd7f134571b389d30b
Wilfried <wilfried@web-id.fr>
2021-02-23 18:47:13 +0100

    :memo: Update README.md

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
-# angular-battle
+# Angular Battle
+
+A battle board game made with Angular 1.
+
+![Angular Battle](screen.png)
 
 ## Installation
 
 ```shell
 npm install
 ```
+
+## Build
+
+```shell
+gulp
+```
+
+To run both `gulp watch` and `gulp serve` commands
+
+Then go to http://localhost:4000
+
+OR
+
+```shell
+gulp watch
+```
+
+If you only want to build file while working on it
+
+OR
+
+```shell
+gulp serve
+```
+
+If you only want to play 
--- gulpfile.js
+++ gulpfile.js
@@ -1,65 +1,80 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var _sass = require('gulp-sass');
 var connect = require('gulp-connect');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var browserify = require('browserify');
+var _browserify = require('browserify');
 var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
 
-// gulp go
-gulp.task('go', function () {
+// gulp serve
+function serve() {
     connect.server({
         root: 'public',
         port: 4000
     });
-});
+}
 
 // gulp cards
-gulp.task('cards', function(){
-    return gulp.src([ './app/cards/*.js' ])
+function cards() {
+    return gulp.src(['./app/cards/*.js'])
         .pipe(concat('cards.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./public/js'));
-});
+}
 
 // gulp engine
-gulp.task('engine', function(){
-    return gulp.src([ './app/engine/*.js' ])
+function engine() {
+    return gulp.src(['./app/engine/*.js'])
         .pipe(concat('engine.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./public/js'));
-});
+}
 
 // gulp sass
-gulp.task('sass', function() {
-    return sass('sass/style.scss')
-        .pipe(gulp.dest('public/css'))
-});
+function sass() {
+    return gulp.src('sass/style.scss')
+        .pipe(_sass().on('error', _sass.logError))
+        .pipe(gulp.dest('public/css'));
+}
 
 // gulp browserify
-gulp.task('browserify', function() {
+function browserify() {
     // TODO Minify public/main.js file
     // Grabs the app.js file
-    return browserify('./app/app.js')
+    return _browserify('./app/app.js')
         // bundles it and creates a file called main.js
         .bundle()
         .pipe(source('main.js'))
         // saves it the public/js/ directory
         .pipe(gulp.dest('./public/js/'));
-});
+}
 
 // gulp watch
 // -> gulp browserify && gulp sass
-gulp.task('watch', function() {
+function watch() {
     gulp.watch('app/**/*.js', ['browserify']);
     // Watches for changes in style.sass and runs the sass task
     gulp.watch('sass/style.scss', ['sass']);
     // Watch other JS modification
     gulp.watch('app/cards/*', ['cards']);
     gulp.watch('app/engine/*', ['engine']);
-});
+}
 
 // gulp
-// -> gulp go && gulp watch
-gulp.task('default', ['go', 'watch']);
+// -> gulp serve && gulp watch
+var build = gulp.series(serve, watch);
+
+/*
+ * You can use CommonJS `exports` module notation to declare tasks
+ */
+exports.serve = serve;
+exports.cards = cards;
+exports.engine = engine;
+exports.sass = sass;
+exports.browserify = browserify;
+exports.watch = watch;
+
+/*
+ * Define default task that can be called by just running `gulp` from cli
+ */
+exports.default = build;
